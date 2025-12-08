747679f5f0c9e503e59947f98013262c84b6e24d
zimbinmusic-lang <zimbinmusic@gmail.com>
2025-12-08 12:54:34 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,28 @@
+// data.js - This file holds all your website content
+
+const siteData = {
+    profile: {
+        name: "Nazim Mubin",
+        initials: "NM",
+        image: "https://ui-avatars.com/api/?name=Nazim+Mubin&background=6366f1&color=fff&size=256",
+        bio_p1: "Nazim Mubin is a passionate musical artist creating soulful melodies and engaging content. With a unique blend of modern sounds and heartfelt lyrics, Nazim brings a fresh perspective to the music scene.",
+        bio_p2: "From his latest single \"Aaja Heeriye\" to his growing presence on YouTube, Nazim is dedicated to connecting with listeners through the power of music. Join the journey by following on Spotify and subscribing on YouTube."
+    },
+    socials: {
+        youtube: "https://www.youtube.com/@nazimxmubin",
+        instagram: "https://www.instagram.com/",
+        spotify: "https://open.spotify.com/artist/3AeJN7kI13TEa7ItCJ9s6P"
+    },
+    latest_video: {
+        url: "https://www.youtube.com/embed/8teszBXo9zg?si=_7LxM6OXsnn6itQR"
+    },
+    featured_song: {
+        title: "Aaja Heeriye",
+        status: "Out Now",
+        apple_link: "https://music.apple.com/us/album/aaja-heeriye-single/1857156505",
+        yt_music_link: "https://music.youtube.com/watch?v=RYFV15aYEG4&si=1C_TBg3oyq8tfsMd"
+    },
+    popular_track: {
+        spotify_embed: "https://open.spotify.com/embed/track/0VPdtAOwfk4tKJYSAOX9vA?utm_source=generator&theme=0"
+    }
+};
